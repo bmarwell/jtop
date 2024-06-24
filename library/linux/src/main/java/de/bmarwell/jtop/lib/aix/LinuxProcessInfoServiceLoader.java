40e91be802747b5eba6eba8fb81c1177ea6d9ad7
Benjamin Marwell <bmarwell@apache.org>
2024-06-24 09:57:49 +0200

    aix support; jreleaser distro

--- library/linux/src/main/java/de/bmarwell/jtop/lib/aix/LinuxProcessInfoServiceLoader.java
+++ library/linux/src/main/java/de/bmarwell/jtop/lib/aix/LinuxProcessInfoServiceLoader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.bmarwell.jtop.lib.linux;
+package de.bmarwell.jtop.lib.aix;
 
 import de.bmarwell.jtop.lib.api.spi.ProcessH;
 import de.bmarwell.jtop.lib.api.spi.ProcessInfoServiceLoader;
